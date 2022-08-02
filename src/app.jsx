98e6431cde5502501c5b71d7311ae83e6d28a340
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-08-02 10:44:08 +0500

    remove bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Header from "./components/header/header";
 import Buttons from "./components/buttons/buttons";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./base.scss";
 
 class App extends Component {
