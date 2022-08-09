a889889ae8c003d617bfc08ef71d0f2a7501396d
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-08-09 20:39:50 +0500

    finish code

--- src/app.jsx
+++ src/app.jsx
@@ -1,100 +1,46 @@
 import { Component } from "react";
-import NavBar from "./components/nav-bar";
-import Genres from "./components/genres";
-import Movies from "./components/movies";
-import Loader from "./components/loader";
-import Total from "./components/total";
-import _ from "lodash";
-import { paginate } from "./helpers/paginate";
-import { fakeGetMovies } from "./services/fake-get-movies";
-import { fakeGetGenres } from "./services/fake-get-genres";
+import Table from "./components/table";
+import "./base.scss";
 
 class App extends Component {
-  state = {
-    loading: true,
-    movies: [],
-    genres: [],
-    genreID: "all",
-    currentPage: 1,
-    pageSize: 3,
-    sortedMovies: [],
-    tempMovies: [],
+  handleThLength = (number) => {
+    const thead = [];
+    for (let i = 1; i < number; i++) {
+      thead.push(<th>Column {i}</th>);
+    }
+    return thead;
   };
 
-  handleSelectGenre = (newGenreID) => {
-    this.setState({
-      genreID: newGenreID,
-      currentPage: 1,
-      movies: this.state.tempMovies,
-    });
+  handleTableCount = (count) => {
+    const table = [];
+    for (let i = 0; i < count; i++) {
+      table.push(
+        <tr>
+          <td>Edrward {i}</td>
+          <td>32</td>
+          <td>London Park no. {i}</td>
+          <td>London Park no. {i}</td>
+          <td>London Park no. {i}</td>
+          <td>London Park no. {i}</td>
+          <td>London Park no. {i}</td>
+          <td>London Park no. {i}</td>
+          <td>London Park no. {i}</td>
+          <td>London Park no. {i}</td>
+          <td>London Park no. {i}</td>
+        </tr>
+      );
+    }
+    return table;
   };
-
-  handlePageChange = (newPage) => {
-    this.setState({ currentPage: newPage });
-  };
-
-  handleLike = (movieID) => {
-    const movies = [...this.state.movies];
-    const movie = movies.find((movie) => movie._id === movieID);
-
-    movie.isLiked = !movie.isLiked;
-    this.setState({ movies });
-  };
-
-  handleDeleteMovie = (movieID) => {
-    const movies = this.state.movies.filter((movie) => movie._id !== movieID);
-    this.setState({ movies });
-  };
-
-  handleSort = (type) => {
-    const tempArr = this.state.movies;
-    const orderedItems = _.orderBy(tempArr, [type]);
-    this.setState({ tempMovies: [...tempArr], movies: orderedItems });
-  };
-
-  componentDidMount() {
-    const movies = fakeGetMovies();
-    const genres = fakeGetGenres();
-    this.setState({ movies });
-    genres.unshift({ name: "All", _id: "all" });
-    setTimeout(() => this.setState({ loading: false, movies, genres }), 1000);
-  }
 
   render() {
-    if (this.state.loading) return <Loader />;
-    const { movies, genres, genreID, pageSize, currentPage } = this.state;
-    const filteredMovies = movies.filter(
-      (movie) => genreID === "all" || movie.genre._id === genreID
-    );
-    const paginatedMovies = paginate(filteredMovies, pageSize, currentPage);
-    const total = filteredMovies.length;
-    
     return (
-      <>
-        <NavBar />
-        <div className="container pt-4">
-          <div className="row d-flex">
-            <Genres
-              genres={genres}
-              genreID={genreID}
-              onSelect={this.handleSelectGenre}
-            />
-            <div className="col">
-              <Total total={total} />
-              <Movies
-                currentPage={currentPage}
-                pageSize={pageSize}
-                onPageChange={this.handlePageChange}
-                movies={paginatedMovies}
-                onLike={this.handleLike}
-                total={total}
-                onDeleteMovie={this.handleDeleteMovie}
-                onSorted={this.handleSort}
-              />
-            </div>
-          </div>
-        </div>
-      </>
+      <div className="wrapper">
+        <Table
+          onThLength={this.handleThLength}
+          onTableCount={this.handleTableCount}
+        />
+      </div>
     );
   }
 }
---
+++ src/components/table.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import "./table.scss";
+
+function Table({ onThLength, onTableCount }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Full Name</th>
+          <th>Age</th>
+          {onThLength(10)}
+        </tr>
+      </thead>
+      <tbody>{onTableCount(30)}</tbody>
+    </table>
+  );
+}
+
+Table.propTypes = {
+  onThLength: PropTypes.func.isRequired,
+  onTableCount: PropTypes.func.isRequired,
+};
+
+export default Table;
